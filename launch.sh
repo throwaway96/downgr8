8e547efdccb9caf87df3fcbc67fbc5941564acf2
throwaway96 <68320646+throwaway96@users.noreply.github.com>
2024-03-10 16:02:41 -0400

    launch.sh: improve reliability after elevation
    Also add some comments.

--- launch.sh
+++ launch.sh
@@ -15,10 +15,18 @@ toast() {
 
 toast 'Starting...'
 
+# use Homebrew Channel's elevate-service on self if not already running as root
 if ! luna-send-pub -n 1 "luna://${app_id}.service/elevate" '{}'; then
     toast 'Luna call failed: elevate.'
 fi
 
+# make sure the service uses the new permissions if it was elevated
+if ! luna-send-pub -n 1 "luna://${app_id}.service/quit" '{}'; then
+    toast 'Luna call failed: quit.'
+fi
+
+# this is not strictly necessary, as the handler will run 'pidof update' itself
+# when "pid" is not passed
 pid="$(pidof 'update')"
 payload="$(printf '{"pid":"%u"}' "${pid}")"
 
@@ -26,6 +34,9 @@ if ! luna-send-pub -n 1 "luna://${app_id}.service/patch" "${payload}"; then
     toast 'Luna call failed: patch.'
 fi
 
+# restart securitymanager
 if ! luna-send-pub -n 1 "luna://${app_id}.service/restart" '{}'; then
     toast 'Luna call failed: restart.'
 fi
+
+# vim: noet:ts=4:sw=9:
